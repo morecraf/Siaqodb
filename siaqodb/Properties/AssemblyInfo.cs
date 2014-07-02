3d8436fddea79782cdbbfb2492bd9430bb299d9f
cristiursachi <cristiursachi@gmail.com>
2014-07-02 13:01:20 +0300

    missing assemblyInfo

---
+++ siaqodb/Properties/AssemblyInfo.cs
@@ -0,0 +1,41 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("siaqodb")]
+[assembly: AssemblyDescription("NoSQL embedded database for .NET")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Dotissi")]
+[assembly: AssemblyProduct("siaqodb")]
+[assembly: AssemblyCopyright("Copyright © Dotissi 2014")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+#if UNITY3D || CF 
+#else
+//[assembly:AssemblyKeyFile("siaqodb.pfx")]
+#endif
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("6499f830-6a14-4472-9eb7-dabbf0470ceb")]
+
+//[assembly:InternalsVisibleTo("SiaqodbManager")]
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+[assembly: AssemblyVersion("4.0.1.0")]
+
